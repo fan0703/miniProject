e7dd44b997ecad95f38a8fee7f9f144c19ac965a
Fan Yang <yangfanhewangzheng@gmail.com>
2023-08-07 00:54:55 -0400

    adding for loop

--- script.js
+++ script.js
@@ -6,46 +6,6 @@ const usSpaceship = {
     Accuracy :.7,
 }
 
-function attack(){
-    if(Math.random() >= 0.7){
-        while(alienShips[0].Hull > 0)
-        return ("You hit it!")
-    }else{
-    return ('missed')
-    }
-}
-
-function getShooted(){
-    if(Math.random() < 0.7){
-        return ('You have been hit!')
-        Hull = Hull - (alienShips[0].FirePower)
-        if(Hull = 0){
-            return ("USS Schwarzenegger is destroyed, Game over!")
-        }
-    }
-
-}
-  
-function makeChoice(){    
-    if(alienShips[0].Hull = 0){
-    return('retreat or continue attack alienShips[1]')
-    } 
-        if(select === "retreat"){
-            return("Game is over!")
-        } 
-        if(select === "continue attack alienShips[1]"){
-            usSpaceship.attack() 
-        }
-}
-
-usSpaceship.attack()
-usSpaceship.getShooted()
-usSpaceship.makeChoice()
-
-
-  
-     
-
 //creat alien spaceship object
 
 class AlienSpaceship {
@@ -54,26 +14,7 @@ class AlienSpaceship {
     this.Hull = Math.floor(Math.random()*4+3);
     this.FirePower = Math.floor(Math.random()*3+2);
     this.Accuracy = Math.random(); //how to let it in 0.6-0.8
-}
-
-getShooted(){
-        if(Math.random() < alienShips[0].Accuracy){
-            return ('You have been hit!')
-            Hull = Hull - 5
-            if(Hull = 0){
-                return ("alienShips[0] is destroyed")
-            }
-        }
-    
-    }
-attack(){
-    if(Math.random() >= alienShips[0].Accuracy){
-        while(usSpaceship.Hull > 0)
-        return ("You hit it!")
-    }else{
-        return ('missed')
-        }
-    }
+ }
 }
 
 class ShipFactory{
@@ -100,3 +41,74 @@ ship1.generateShip('alienShip4', Math.floor(Math.random()*4+3),Math.floor(Math.r
 console.log(ship1.alienShips)
 ship1.generateShip('alienShip5', Math.floor(Math.random()*4+3),Math.floor(Math.random()*3+2),Math.random())
 console.log(ship1.alienShips)
+const enemyShips = ship1.alienShips;
+
+for(i = 0; i < enemyShips.length; i++){
+    function attack(){
+    if(Math.random() >= usSpaceship.Accuracy){
+        while(enemyShips[i].Hull > 0)
+        enemyShips.Hull -= usSpaceship.FirePower
+        console.log ("You hit it!")
+    }else{
+       console.log ('missed')
+      }
+    }
+  
+    function getShooted(){
+      if(Math.random() >= enemyShips[i].Accuracy){
+          console.log ('You have been hit!')
+          usSpaceship.Hull -= enemyShips[i].FirePower
+          if(usSpaceship.Hull = 0){
+            console.log ("USS Schwarzenegger is destroyed, Game over!")
+          }
+       }
+  
+    }
+  
+    function makeChoice(){    
+      if(enemyShips[i].Hull = 0){
+      console.log(`${enemyShips[i]} + is destroyed`)
+      console.log ('retreat or continue attack enemyShips[i++]')
+      } 
+        if(select === "retreat"){
+              return("Game is over!")
+        } 
+        if(select === "continue attack enemyShips[i++]"){
+            while(enemyShips[i++].Hull > 0){
+            return attack() //???
+            }if(enemyShips[i++].Hull = 0){
+                console.log("You win!")
+            }
+        }
+    }
+}
+  
+
+
+
+
+
+
+
+
+// getShooted(){
+//         if(Math.random() < alienShips[0].Accuracy){
+//             return ('You have been hit!')
+//             Hull = Hull - 5
+//             if(Hull = 0){
+//                 return ("alienShips[0] is destroyed")
+//             }
+//         }
+    
+//     }
+// attack(){
+//     for(i = 0; i < alienShips.length; i++)
+//     if(Math.random() >= alienShips[i].Accuracy){
+//         // while(usSpaceship.Hull > 0)
+//         return ("You hit USS Schwarzenegger!")
+//     }else{
+//         return ('missed')
+//         }
+//     }
+// }
+
